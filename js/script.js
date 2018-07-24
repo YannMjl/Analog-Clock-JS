6972d315ba7e358fb0cc0fd4723bc89b8b585e0a
Yann Mulonda <yannmjl@gmail.com>
2018-07-24 17:47:58 -0500

    completed project

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// constant variables
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+var date = new Date();
+console.log(date);
+
+// now let get the hour, minute and second
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("The curent time is: ", hr + ":" + min + ":" + sec);
+
+// let convert hour, minute and seconds into degree to be display on the clockbox
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+let secPosition = sec * 360 / 60;
+
+function runTheClock() {
+
+  hrPosition += 3/360;
+  minPosition += 6/60;
+  secPosition += 6;
+  
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock, 1000);
