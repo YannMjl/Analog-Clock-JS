2ff2ba2283af3320c1767dca894c94ed03f41667
Yann Mulonda <yannmjl@gmail.com>
2018-07-25 14:08:00 -0500

    clean up js code

--- js/script.js
+++ js/script.js
@@ -22,10 +22,11 @@ function runTheClock() {
   hrPosition += 3/360;
   minPosition += 6/60;
   secPosition += 6;
-  
+
   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
   MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
   SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
+// run runTheClock function every second
 var interval = setInterval(runTheClock, 1000);
