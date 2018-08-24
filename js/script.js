bff749228911b59f68cd9b48ca6c60190cef0aee
Yann Mulonda <yannmjl@gmail.com>
2018-08-23 23:46:31 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 var date = new Date();
-console.log(date);
 
 let hr = date.getHours();
 let min = date.getMinutes();
